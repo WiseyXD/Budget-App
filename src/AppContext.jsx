b3cde2a4237ae92b5ac84163fc2abaff2b48cde8
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-07-29 10:29:47 +0530

    feat : Done with the Edit Budget

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -19,6 +19,12 @@ const AppReducer = (state, action) => {
 				],
 			};
 
+		case "NEW_BUDGET":
+			return {
+				...state,
+				budget: action.payload,
+			};
+
 		default:
 			return state;
 	}
--- src/Components/Budget.jsx
+++ src/Components/Budget.jsx
@@ -1,22 +1,52 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../AppContext";
 export default function Budget() {
 	const { budget, dispatch } = useContext(AppContext);
+	let [newBudget, setNewBudget] = useState(false);
+	let [getBudget, setGetBudget] = useState(null);
 	function handleNewBudget() {
-		// 	dispatch({
-		// 		type:"NEW_BUDGET",
-		// 		payload:
-		// 	})
+		setNewBudget(true);
+		console.log("clicked");
 	}
+	function saveNewBudget() {
+		setNewBudget(false);
+
+		dispatch({
+			type: "NEW_BUDGET",
+			payload: getBudget,
+		});
+	}
+
 	return (
 		<div className="alert alert-success d-flex justify-content-between align-items-center">
-			<h5>Budget: ₹ {budget}</h5>
-			<button
-				className="btn btn-outline-primary btn-sm m-0"
-				onClick={() => handleNewBudget}
-			>
-				Edit Budget
-			</button>
+			{newBudget ? (
+				<>
+					<input
+						type="number"
+						name=""
+						id=""
+						placeholder="Set Budget"
+						onChange={(e) => setGetBudget(e.target.value)}
+						min={1}
+					/>
+					<button
+						className="btn btn-outline-primary btn-sm m-0"
+						onClick={saveNewBudget}
+					>
+						Save
+					</button>
+				</>
+			) : (
+				<>
+					<h5>Budget: ₹ {budget}</h5>
+					<button
+						className="btn btn-outline-primary btn-sm m-0"
+						onClick={handleNewBudget}
+					>
+						Edit Budget
+					</button>
+				</>
+			)}
 		</div>
 	);
 }
